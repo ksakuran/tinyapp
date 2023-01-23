9a80c509fae1d020398c1789f66558dd93848c31
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-23 23:25:07 +0000

    added a /hello path, with an html coded hello world

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -8,15 +9,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
-});
-
 // "/" root path
 app.get("/", (request, response) => {
   response.send("Hello!");
 });
 
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
