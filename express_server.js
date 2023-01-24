f9b767068d8bdc471746cbc23fe5df81afaab486
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-24 01:21:02 +0000

    added a handler for /urls, and added a res.render

--- express_server.js
+++ express_server.js
@@ -12,17 +12,21 @@ const urlDatabase = {
 };
 
 // "/" root path
-app.get("/", (request, response) => {
-  response.send("Hello!");
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (request, response) => {
-  response.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
