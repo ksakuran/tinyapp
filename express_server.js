012fe93dde87a3da716cfb0080fd0201319a577e
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-24 04:11:20 +0000

    refactored and linted the code for alphanumeric string generator function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
-const generateRandomString() {
-  let randomString = ""
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
-    for (let i = 0; i <= 6; i++) {
-      randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+const generateRandomString = () => {
+  let randomString = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return randomString;
 };
