5e349cfdd90b313fcfc17024da8adf6e57cb43f3
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-25 03:52:26 +0000

    added a cookie parser middleware, added the cookies to template vars, to pass in rendering to the html pages partial header, added the username to display beside login form

--- express_server.js
+++ express_server.js
@@ -12,12 +12,14 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
 const PORT = 8080; //default port on 8080;
 
 app.set("view engine", "ejs");
 
 //middleware, parses body data
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -59,12 +61,19 @@ app.post("/login", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });  
 
@@ -74,7 +83,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
